c43a59a8d0d33b9a6f75985b01f7e767589c51d9
Harsh Choubey <harshchoubey712@gmail.com>
2024-05-25 22:02:57 +0200

    Deploy changes

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OPENAI_KEY } from "./constants";
 
 const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_KEY,
+  apiKey: OPENAI_KEY,
   dangerouslyAllowBrowser: true,
 });
 
