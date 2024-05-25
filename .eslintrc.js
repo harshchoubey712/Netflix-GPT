c04d93b847aa1fc8b71f575dfbf3206bcc0a8b0f
Harsh Choubey <harshchoubey712@gmail.com>
2024-05-25 22:24:57 +0200

    Deploy changes

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // existing configuration
+  rules: {
+    "no-unused-vars": "warn",
+    "react-hooks/exhaustive-deps": "warn", // example for React projects
+    // other rules
+  },
+};
